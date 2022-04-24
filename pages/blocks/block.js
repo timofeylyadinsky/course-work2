531a538268ce156583da2bcda19d593379312381
Timofey Lyadinsky <tima.led12@gmail.com>
2022-04-24 23:31:02 +0300

    feat: add base animation

--- pages/blocks/block.js
+++ pages/blocks/block.js
@@ -1,6 +1,7 @@
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
+const g = 9.81; //Gravitational acceleration constanta
 
 var block = {
     x: canvas.width / 2,
@@ -13,6 +14,7 @@ var block = {
         ctx.lineWidth = 5;
         ctx.stroke();
         ctx.beginPath();
+        ctx.fillStyle = "black";
         ctx.arc(this.x, this.y, this.radius/2-5, 0, Math.PI * 2, true);
         ctx.closePath();
         ctx.fill();
@@ -28,6 +30,7 @@ var ropeLeft = {
         ctx.lineWidth = 5;
         ctx.moveTo(this.x, this.y);
         ctx.lineTo(this.x, this.yEnd);
+        ctx.fillStyle = "black";
         ctx.closePath();
         ctx.stroke();
     }
@@ -41,6 +44,7 @@ var ropeRight = {
         ctx.lineWidth = 5;
         ctx.moveTo(this.x, this.y);
         ctx.lineTo(this.x, this.yEnd);
+        ctx.fillStyle = "black";
         ctx.closePath();
         ctx.stroke();
     }
@@ -50,10 +54,11 @@ var loadLeft = {
     x: ropeLeft.x,
     y: ropeLeft.yEnd,
     size: 20,
+    m: 10,
     draw: function(){
         ctx.beginPath();
         ctx.fillStyle = "blue";
-        ctx.fillRect(this.x - 2 * this.size, this.y - 2 * this.size, 4 * this.size, 4 * this.size,)
+        ctx.fillRect(this.x - 2 * this.size, this.y, 4 * this.size, 4 * this.size,)
         ctx.closePath();
     }
 } 
@@ -62,19 +67,71 @@ var loadRight = {
     x: ropeRight.x,
     y: ropeRight.yEnd,
     size: 20,
+    m: 10,
     draw: function(){
         ctx.beginPath();
         ctx.fillStyle = "red";
-        ctx.fillRect(this.x - 2 * this.size, this.y - 2 * this.size, 4 * this.size, 4 * this.size,)
+        ctx.fillRect(this.x - 2 * this.size, this.y, 4 * this.size, 4 * this.size,)
         ctx.closePath();
     }
 } 
+drawElements();
 
-block.draw();
-ropeLeft.draw();
-ropeRight.draw();
-loadLeft.draw();
-loadRight.draw();
+function drawElements(){
+    block.draw();
+    ropeLeft.draw();
+    ropeRight.draw();
+    loadLeft.draw();
+    loadRight.draw();
+}
+
+let a = 1;
+let yInvisible = block.y + block.radius;
+function start(){
+    validate();
+    if(loadLeft.m != loadRight.m){
+        a = (g * (loadLeft.m - loadRight.m)/(loadLeft.m + loadRight.m));
+        if(loadRight.y-a <= yInvisible || loadLeft.y+a <= yInvisible) {
+            if(loadRight.y < loadLeft.y && loadRight.y > yInvisible){
+                ctx.clearRect(0,0,innerWidth,innerHeight);
+                ropeLeft.yEnd+=loadRight.y-yInvisible;
+                loadLeft.y+=loadRight.y-yInvisible;
+                ropeRight.yEnd=yInvisible+2;
+                loadRight.y=yInvisible+2;
+                drawElements();
+            }
+            if(loadRight.y > loadLeft.y && loadLeft.y > yInvisible){
+                ctx.clearRect(0,0,innerWidth,innerHeight);
+                ropeRight.yEnd+=loadLeft.y-yInvisible;
+                loadRight.y+=loadLeft.y-yInvisible;
+                ropeLeft.yEnd=yInvisible+2;
+                loadLeft.y=yInvisible+2;
+                drawElements();
+            }
+            return;
+        }
+        ctx.clearRect(0,0,innerWidth,innerHeight);
+        drawElements();
+        ropeRight.yEnd-=a;
+        loadRight.y-=a;
+        ropeLeft.yEnd+=a;
+        loadLeft.y+=a;
+        if(loadRight.y > yInvisible || loadLeft.y > yInvisible) requestAnimationFrame(start);
+    }
+}
+
+function validate(){
+    let weight1 = parseFloat(document.getElementById("weight1").value);
+    let weight2 = parseFloat(document.getElementById("weight2").value);
+    if(!isNaN(weight1)){
+        if(weight1 > 9999 || weight1 <= 0) alert("Incorrect weight of blue load")
+        else loadLeft.m = weight1;
+    }
+    if(!isNaN(weight2)){
+        if(weight2 > 9999 || weight2 <= 0) alert("Incorrect weight of red load")
+        else loadRight.m = weight2;
+    }
+}
 
 
 /*
