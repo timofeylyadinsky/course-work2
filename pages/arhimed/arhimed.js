82d231b2323d33342da357056e577bb9f68b1670
Timofey Lyadinsky <tima.led12@gmail.com>
2022-05-13 00:03:21 +0300

    feat: add arhimed law logic

--- pages/arhimed/arhimed.js
+++ pages/arhimed/arhimed.js
@@ -26,11 +26,12 @@ var goods = {
     volume: 0.1,
     volumeInWater: 1,
     volumeOutWater: 0,
-    size: 0,
+    size: 46,
+    color: "black",
     draw: function(){
-        this.size = Math.pow(this.volume, 1/3) * 100;
+        this.size = Math.pow(this.volume, 1/3) * 100;        
         ctx.beginPath();
-        ctx.fillStyle = "black";
+        ctx.fillStyle = this.color;
         ctx.fillRect(this.x - this.size / 2, this.y - this.size / 2, this.size, this.size);
         ctx.fill();
         ctx.closePath();
@@ -39,12 +40,85 @@ var goods = {
 
 //alert(Math.pow(8, 1/3));
 
-drawElements();
+//drawElements();
 
 function drawElements(){
+    ctx.clearRect(0,0,innerWidth,innerHeight);
+    addColorToGoods();
     water.draw();
     goods.draw();
 }
+
+function setDensity(radio){
+    const type = parseInt(radio.value);
+    if(!isNaN(type)){
+        goods.density = type;
+    }else{
+        alert("Error");
+    }
+    drawElements();
+}
+
+function addColorToGoods(){
+    ctx.clearRect(0,0,innerWidth,innerHeight);
+    switch(goods.density){
+        case 240: 
+        //ctx.drawImage(img, goods.x, goods.y, 10, 10);
+        probkaPattern = ctx.createPattern(img, "repeat");
+        ctx.fillStyle = probkaPattern; 
+        ctx.fill();
+        goods.color = probkaPattern;
+        break;
+        case 700: 
+        var gradient = ctx.createRadialGradient(goods.x, goods.y, goods.size / 4, goods.x, goods.y, goods.size / 2);
+
+        // Add three color stops
+        gradient.addColorStop(0, '#B5885F');
+        gradient.addColorStop(.8, '#E0BC5F');
+        gradient.addColorStop(1, '#8C623A');
+        goods.color = gradient;
+        //drawElements();
+        break;
+        case 2700: 
+        var gradient = ctx.createLinearGradient(goods.x - 0.5 * goods.size, goods.y - 0.5 * goods.size, goods.x + goods.size * 0.5, goods.y + goods.size * 0.5);
+        gradient.addColorStop(0, '#FFFAF0');
+        gradient.addColorStop(.1, '#CDCDC8');
+        gradient.addColorStop(.3, '#BEBEC8');//#BEBEC8
+        gradient.addColorStop(.4, '#FFFAF0');
+        gradient.addColorStop(.7, '#FFFAF0');
+        gradient.addColorStop(.8, '#CDCDC8');
+        gradient.addColorStop(.9, '#BEBEC8');
+        gradient.addColorStop(1, '#FFFAF0');
+        goods.color = gradient;
+        break;
+        case 1800: 
+        goods.color = "#854232"
+        break;
+        case 8900: 
+        var gradient = ctx.createLinearGradient(goods.x - 0.5 * goods.size, goods.y - 0.5 * goods.size, goods.x + goods.size * 0.5, goods.y + goods.size * 0.5);
+        gradient.addColorStop(0, '#FFFAF0');
+        gradient.addColorStop(.1, '#CDCDC8');
+        gradient.addColorStop(.3, '#BEBEC8');//#BEBEC8
+        gradient.addColorStop(.4, '#FFFAF0');
+        gradient.addColorStop(.7, '#FFFAF0');
+        gradient.addColorStop(.8, '#CDCDC8');
+        gradient.addColorStop(.9, '#BEBEC8');
+        gradient.addColorStop(1, '#FFFAF0');
+        goods.color = gradient;
+        break;
+    }
+}
+
+var img = new Image();
+var probkaPattern;
+img.onload = function() {
+    ctx.drawImage(img, 10, 10, goods.size-10, goods.size-10, goods.x, goods.y, goods.size-20, goods.size-20);
+    probkaPattern = ctx.createPattern(img, "repeat");
+    ctx.fillStyle = probkaPattern; 
+    ctx.fill();
+    drawElements();
+};
+img.src = "../../assets/probka.jpg";
 
 let a = 0, v = 0;
 
@@ -56,25 +130,38 @@ function start(){
     fT = goods.volume * goods.density * g;
     let mGoods = goods.volume * goods.density; 
     a = (fT - fA) / mGoods / 100;
-    if(Math.abs(fT-fA) <= 0.0001){
+    if(Math.abs(fT-fA) <= 0.01){
+        v = 0;
         return;
     }
-    //v+=a;
-    if(goods.y + 0.5 * goods.size > canvas.height){
+   
+    if(goods.y + 0.5 * goods.size > canvas.height && a > 0){
+        
         goods.y = canvas.height - 0.5 * goods.size;
         a = 0;
+        v = 0;
         ctx.clearRect(0,0,innerWidth,innerHeight);
         drawElements();
         return;
     };
     if(goods.y - goods.size * 0.5 <= water.y){
-        let heightAbove = 0;
-        heightAbove = water.y - goods.y + goods.size * 0.5;
-        heightAbove /= goods.size;
-        goods.volumeInWater = 1 - heightAbove;
-        goods.volumeOutWater = 0 + heightAbove;
+        if(a <= 0){
+            let heightAbove = 0;
+            heightAbove = water.y - goods.y + goods.size * 0.5;
+            heightAbove /= goods.size;
+            goods.volumeInWater = 1 - heightAbove;
+            goods.volumeOutWater = 0 + heightAbove;
+            v = 5 * a;
+        }else{
+            let heightAbove = 0;
+            heightAbove = water.y - goods.y + goods.size * 0.5;
+            heightAbove /= goods.size;
+            goods.volumeInWater = 1 - heightAbove;
+            goods.volumeOutWater = 0 + heightAbove;
+        }
     }
-    goods.y+=a;
+    v+=a;
+    goods.y+=v;
     ctx.clearRect(0,0,innerWidth,innerHeight);
     drawElements();
     requestAnimationFrame(start)
